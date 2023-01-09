511dca2c825a5e1259e6ff3ee34b96f4f3b867c6
corentinleandre <corentinleandre+github@gmail.com>
2023-01-09 22:40:28 +0100

    Setting up models

--- chat_server/chat/models.py
+++ chat_server/chat/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Chat(models.Model):
+    creation_date = models.DateTimeField()
+
+class User(models.Model):
+    username = models.CharField(max_length=40)
+    rights = models.IntegerField()
+
+class Message(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    content = models.CharField(max_length=400)
+    publication_date = models.DateTimeField()
