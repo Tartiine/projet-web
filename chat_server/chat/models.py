9607ffb326739e6b8499ea3a495d269d61a507fa
corentinleandre <corentinleandre+github@gmail.com>
2023-01-12 21:18:38 +0100

    Test chat and messages + to_dict in Message

--- chat_server/chat/models.py
+++ chat_server/chat/models.py
@@ -1,16 +1,30 @@
 from django.db import models
+from django.forms import model_to_dict
 
 # Create your models here.
 
-class Chat(models.Model):
-    creation_date = models.DateTimeField()
 
 class User(models.Model):
     username = models.CharField(max_length=40)
     rights = models.IntegerField()
+
+
+class Chat(models.Model):
+    name = models.CharField(max_length=40)
+    creator = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    creation_date = models.DateTimeField()
+
 
 class Message(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
     content = models.CharField(max_length=400)
     publication_date = models.DateTimeField()
+
+    def to_dict(self):
+        dicmessage = {}
+        dicmessage['author'] = model_to_dict(self.author)
+        dicmessage['chat'] = model_to_dict(self.chat)
+        dicmessage['content'] = self.content
+        dicmessage['publication_date'] = self.publication_date
+        return dicmessage
--- chat_server/chat/views.py
+++ chat_server/chat/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.http import HttpResponse, JsonResponse
+from django.forms import model_to_dict
+
+from chat.models import Message, Chat, User
+
 
 # Create your views here.
+
+def init(request):
+    user1 = User(username="user1", rights=1)
+    user1.save()
+    user2 = User(username="user2", rights=1)
+    user2.save()
+    chat = Chat(name="general", creator=user1, creation_date=timezone.now())
+    chat.save()
+    m1 = Message(author=user1, chat=chat,  content="The first of a lot of random messages", publication_date=timezone.now())
+    m1.save()
+    m2 = Message(author=user1, chat=chat, content="The second of a lot of random messages", publication_date=timezone.now())
+    m2.save()
+    m3 = Message(author=user2, chat=chat, content="The third of a lot of random messages", publication_date=timezone.now())
+    m3.save()
+    m4 = Message(author=user1, chat=chat, content="The fourth of a lot of random messages", publication_date=timezone.now())
+    m4.save()
+    m5 = Message(author=user2, chat=chat, content="The fifth of a lot of random messages", publication_date=timezone.now())
+    m5.save()
+    m6 = Message(author=user2, chat=chat, content="The sixth of a lot of random messages", publication_date=timezone.now())
+    m6.save()
+    m7 = Message(author=user1, chat=chat, content="The seventh of a lot of random messages", publication_date=timezone.now())
+    m7.save()
+    m8 = Message(author=user1, chat=chat, content="The eighth of a lot of random messages", publication_date=timezone.now())
+    m8.save()
+    m9 = Message(author=user1, chat=chat, content="The ninth of a lot of random messages", publication_date=timezone.now())
+    m9.save()
+    m10 = Message(author=user2, chat=chat, content="The tenth of a lot of random messages", publication_date=timezone.now())
+    m10.save()
+
+
+def gettestchat(request):
+    chat = Chat.objects.get(name="general")
+    messages = chat.message_set.only('author', 'chat', 'content', 'publication_date').all()
+    print(messages)
+
+    data = [message.to_dict() for message in messages]
+    print(data)
+
+    response = JsonResponse({'chatName': chat.name, 'messages': data})
+    return response
