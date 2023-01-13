7a3f748e6b81bf21f8fb4cade239ee1223466b0b
corentinleandre <corentinleandre+github@gmail.com>
2023-01-13 11:35:45 +0100

    getChats and getMessages done

--- chat_server/chat/views.py
+++ chat_server/chat/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+
 from .models import Chat
 
 # Create your views here.
@@ -15,9 +17,23 @@ def thread(request):
     return render(request, 'thread.html')
 
 def getChats(request):
+    print(request)
+    chats = Chat.objects.all()
+    data = [chat.to_dict() for chat in chats]
+    return JsonResponse({'chats' : data})
 
 def getMessages(request):
+    print(request)
+    chat = Chat.objects.get(name="general")
+    messages = chat.message_set.only('author', 'chat', 'content', 'publication_date').all()
+    data = [message.to_dict() for message in messages]
+
+    return JsonResponse({'chat' : chat.name, 'messages':data})
 
 def createChat(request):
+    print(request)
+    return
 
 def saveMessage(request):
+    print(request)
+    return
