9e7d6129e1e85e9654788e58d0a145a337f01633
corentinleandre <corentinleandre+github@gmail.com>
2023-01-13 01:01:52 +0100

    added to_dict to User and Chat

--- chat_server/chat/models.py
+++ chat_server/chat/models.py
@@ -7,12 +7,23 @@ class User(models.Model):
     username = models.CharField(max_length=40)
     rights = models.IntegerField()
 
+    def to_dict(self):
+        dicuser = {}
+        dicuser['username'] = self.username
+        dicuser['rights'] = self.rights
+        return dicuser
+
 class Chat(models.Model):
     name = models.CharField(max_length=40)
     creator = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     creation_date = models.DateTimeField()
 
     def to_dict(self):
+        dicchat = {}
+        dicchat['name'] = self.name
+        dicchat['author'] = model_to_dict(self.creator)
+        dicchat['creation_date'] = self.creation_date
+        return dicchat
 
 class Message(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
