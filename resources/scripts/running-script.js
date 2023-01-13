edc67a0cfd4aa4fa207de3434991a484ada2afb6
corentinleandre <corentinleandre+github@gmail.com>
2023-01-13 17:49:59 +0100

    generateChat works

--- resources/scripts/running-script.js
+++ resources/scripts/running-script.js
@@ -1,7 +1,26 @@
-user = $("username")
+username = $('#username').html()
+if(username != ""){
+    console.log(username)
+}else{
+    console.log("no username")
+}
 
 function generateChat(messages){
-
+    str = ""
+    for(message of messages){
+        str += "<li>\n"
+        if(message.author.username == username){
+            str += "<div class=\"message-me\">\n"
+        }else{
+            str += "<div class=\"message-other\">\n"
+        }
+        str += "<p>" + message.author.username + "</p>\n"
+        str += "<p>" + message.content + "</p>\n"
+        str += "<p>" + message.publication_date + "</p>\n"
+        str += "</div>\n"
+        str += "</li>\n"
+    }
+    return str
 }
 
 
@@ -14,4 +33,5 @@ $.ajax('http://localhost:8000/chat/getMessages', {
 }).done( response => {
     console.log("response gotten")
     console.log(response.messages)
+    $("#conversation-thread").html(generateChat(response.messages))
 })
